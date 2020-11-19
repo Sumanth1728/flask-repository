dccc44da985e869a9f055811cabf8d0c28408a98
Sumanth1728 <73275822+Sumanth1728@users.noreply.github.com>
2020-11-19 10:04:17 +0530

    first commit

---
+++ Base.py
@@ -0,0 +1,90 @@
+from flask import Flask, render_template, session, redirect, url_for, session
+from flask_wtf import FlaskForm
+from wtforms import (StringField, BooleanField, DateTimeField,
+                     RadioField,SelectField,TextField,
+                     TextAreaField,SubmitField)
+from wtforms.validators import DataRequired
+from tables import db, B_Admin ,B_Employee ,B_Customer,B_Customer_transactions,B_Customer_Card_Details
+
+
+app = Flask(__name__)
+# Configure a secret SECRET_KEY
+# We will later learn much better ways to do this!!
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+# Now create a WTForm Class
+# Lots of fields available:
+# http://wtforms.readthedocs.io/en/stable/fields.html
+class LoginForm(FlaskForm):
+    '''
+    This general class gets a lot of form about puppies.
+    Mainly a way to go through many of the WTForms Fields.
+    '''
+    user = StringField('UserName',validators=[DataRequired()])
+    passl  = StringField("Password")
+    submit = SubmitField('Submit')
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    # Create instance of the form.
+    form = LoginForm()
+    # If the form is valid on submission (we'll talk about validation next)
+    if form.validate_on_submit():
+        # Grab the data from the breed on the form.
+
+        try:
+            Admin1 = B_Admin.query.filter_by(A_id=form.user.data).first()
+            print("A")
+            if(Admin1.A_pass==form.passl.data):
+                return redirect(url_for("admin"))
+            else:
+                print("username and pasword doesn't match")
+
+        except Exception as e:
+            try:
+                Emp1 = B_Employee.query.filter_by(E_id=form.user.data).first()
+                if(Emp1.E_pass==form.passl.data):
+                    return redirect(url_for("employee"))
+
+                else:
+                    print("username and pasword doesn't match")
+
+            except Exception as e:
+
+                try:
+                    cmp1 = B_Customer.query.filter_by(C_id=form.user.data).first()
+                    if(cmp.C_pass==form.passl.data):
+                        return redirect(url_for("customer"))
+
+                    else:
+                        print("username and pasword doesn't match")
+
+                except Exception as e:
+
+                    print(e)
+
+
+
+    return render_template('index.html', form=form)
+
+@app.route('/admin')
+def admin():
+
+    return render_template('admin.html')
+
+@app.route('/employee')
+def employee():
+
+    return render_template('employee.html')
+
+@app.route('/customer')
+def customer():
+
+    return render_template('customer.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ tables.py
@@ -0,0 +1,201 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+######################################
+#### SET UP OUR SQLite DATABASE #####
+####################################
+
+
+
+# This grabs our directory
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+# Connects our Flask App to our Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+Migrate(app,db)
+
+#####################################
+####################################
+###################################
+
+# Let's create our first model!
+# We inherit from db.Model class
+
+
+
+class B_Admin(db.Model):
+
+
+    __tablename__ = 'B_Admin'
+
+    A_id = db.Column(db.Text,primary_key=True)
+
+    A_pass=db.Column(db.Text)
+
+    def __init__(self,A_id,A_pass):
+        self.A_id=A_id
+        self.A_pass=A_pass
+
+
+    def __repr__(self):
+        #
+        return f"admin{self.A_id} fetched and password = {self.A_pass}"
+
+
+######################################################################################################################################
+########################################################################################################################################
+
+
+
+class B_Employee(db.Model):
+
+    # If you don't provide this, the default table name will be the class name
+    __tablename__ = 'B_Employee'
+
+    # Now create the columns
+    # Lots of possible types. We'll introduce through out the course
+    # Full docs: http://docs.sqlalchemy.org/en/latest/core/types.html
+
+    #########################################
+    ## CREATE THE COLUMNS FOR THE TABLE ####
+    #######################################
+
+    # Primary Key column, unique id for each puppy
+    E_id = db.Column(db.Text,primary_key=True)
+    E_name = db.Column(db.Text)
+    E_age = db.Column(db.Integer)
+    E_email=db.Column(db.Text)
+    E_address=db.Column(db.Text)
+    E_number = db.Column(db.Integer)
+    E_pass=db.Column(db.Text)
+
+    # This sets what an instance in this table will have
+    # Note the id will be auto-created for us later, so we don't add it here!
+    def __init__(self,E_id,name,age,email,address,number,E_pass):
+
+        self.E_id = E_id
+        self.E_name = name
+        self.E_age = age
+        self.E_email=email
+        self.E_address=address
+        self.E_number=number
+        self.E_pass=E_pass
+
+
+    def __repr__(self):
+
+        # This is the  representation of B_Employee  in the model
+
+        m=[self.E_id,self.E_name,self.E_age,self.E_email,self.E_address,self.E_number,self.E_pass]
+        return f"employee fetched"
+
+
+
+
+
+######################################################################################################################################
+########################################################################################################################################
+
+
+
+
+
+
+class B_Customer(db.Model):
+
+    __tablename__ = 'B_Customer'
+    C_id = db.Column(db.Text,primary_key=True)
+    C_name = db.Column(db.Text)
+    C_age = db.Column(db.Integer)
+    C_email=db.Column(db.Text)
+    C_address=db.Column(db.Text)
+    C_number = db.Column(db.Integer)
+    C_pass=db.Column(db.Text)
+    C_balance=db.Column(db.Float)
+    C_Cards = db.relationship('B_Customer_Card_Details',backref='B_Customer',lazy='dynamic')
+    C_Transacs = db.relationship('B_Customer_transactions',backref='B_Customer',lazy='dynamic')
+
+
+    def __init__(self,C_id,name,age,email,address,number,C_pass,balance):
+
+        self.C_id = C_id
+        self.C_name = name
+        self.C_age = age
+        self.C_email=email
+        self.C_address=address
+        self.C_number=number
+        self.C_pass=C_pass
+        self.C_balance=balance
+
+
+    def __repr__(self):
+        a=[self.C_id,self.C_name,self.C_age,self.C_email,self.C_address,self.C_number,self.C_pass,self.C_balance]
+        return f"customer fetched"
+
+
+
+
+
+######################################################################################################################################
+########################################################################################################################################
+
+
+
+
+class B_Customer_transactions(db.Model):
+    __tablename__ = 'B_Customer_transactions'
+    id = db.Column(db.Integer,primary_key=True)
+    C_id = db.Column(db.Text,db.ForeignKey('B_Customer.C_id'))
+    T_id = db.Column(db.Integer)
+    Card_Number=db.Column(db.Integer,db.ForeignKey('B_Customer_Card_Details.Card_No'))
+
+    def __init__(self,C_id,T_id,Card_Number):
+
+        self.C_id = C_id
+        self.T_id = T_id
+        self.Card_Number=Card_Number
+
+
+    def __repr__(self):
+        return [self.id,self.C_id,self.self.T_id,self.Card_Number]
+
+
+
+######################################################################################################################################
+########################################################################################################################################
+
+
+
+
+class B_Customer_Card_Details(db.Model):
+
+    __tablename__ = 'B_Customer_Card_Details'
+    id = db.Column(db.Integer,primary_key=True)
+    C_id = db.Column(db.Text,db.ForeignKey('B_Customer.C_id'))
+    Card_Type = db.Column(db.Text,unique=True)
+    Card_No = db.Column(db.Integer,unique=True)
+    Card_Activate=db.Column(db.Boolean)
+    Transacs = db.relationship('B_Customer_transactions',backref='B_Customer_Card_Details',lazy='dynamic')
+
+
+
+    def __init__(self,C_id,Card_Type,Card_No,Card_Activate):
+
+        self.C_id = C_id
+        self.Card_Type = Card_Type
+        self.Card_No=Card_No
+        self.Card_Activate=Card_Activate
+
+
+    def __repr__(self):
+        return [self.id,self.self.self.self.C_id,self.self.self.Card_Type,self.self.Card_No,self.Card_Activate]
+
+
+#creating Table instances
+db.create_all()
