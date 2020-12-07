4559d7116d22bcf8fcc3e561d5ba43bcd8796949
jaya1729 <Jayaandsreenu@gmail.com>
2020-12-07 11:33:47 +0530

    changes

--- forms.py
+++ forms.py
@@ -2,20 +2,23 @@ from flask import Flask, render_template, session, redirect, url_for, session
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired,Email,EqualTo,Length
+from tables import db, B_Admin ,B_Employee ,B_Customer,B_Customer_transactions,B_Customer_Card_Details
+
 class createcustomer(FlaskForm):
-    username=StringField('username',validators=[DataRequired(),Length(min=4,max=15)])
-    Email=StringField('Email',validators=[DataRequired(),Email()])
-    phone=StringField('phone',validators=[DataRequired()])
-    state=StringField('state',validators=[DataRequired(),Length(min=3,max=15)])
-    pincode=StringField('pincode',validators=[DataRequired()])
-    address=StringField('address',validators=[DataRequired(),Length(min=10,max=40)])
+    C_id=StringField('ID',validators=[DataRequired(),Length(min=4,max=15)])
+    C_email=StringField('Email',validators=[DataRequired(),Email()])
+    C_number=StringField('phone',validators=[DataRequired()])
+    C_state=StringField('state',validators=[DataRequired(),Length(min=3,max=15)])
+    C_pincode=StringField('pincode',validators=[DataRequired()])
+    C_address=StringField('address',validators=[DataRequired(),Length(min=10,max=40)])
     submit=SubmitField("create user")
 
 class createemloyee(FlaskForm):
-    emp_name=StringField('emp_name',validators=[DataRequired(),Length(min=4,max=15)])
-    emp_Email=StringField('emp_Email',validators=[DataRequired(),Email()])
-    emp_phone=StringField('emp_phone',validators=[DataRequired()])
-    emp_state=StringField('emp_state',validators=[DataRequired(),Length(min=3,max=15)])
-    emp_pincode=StringField('emp_pincode',validators=[DataRequired()])
-    emp_address=StringField('emp_address',validators=[DataRequired(),Length(min=10,max=40)])
+    E_name=StringField('emp_name',validators=[DataRequired(),Length(min=4,max=15)])
+    E_id=StringField('ID',validators=[DataRequired(),Length(min=4,max=15)])
+    E_email=StringField('emp_Email',validators=[DataRequired(),Email()])
+    E_number=StringField('emp_phone',validators=[DataRequired()])
+    E_state=StringField('emp_state',validators=[DataRequired(),Length(min=3,max=15)])
+    E_pincode=StringField('emp_pincode',validators=[DataRequired()])
+    E_address=StringField('emp_address',validators=[DataRequired(),Length(min=10,max=40)])
     submit=SubmitField("create employee")
