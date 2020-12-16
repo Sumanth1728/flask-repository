d0d1791d199f0b513fb6b97eaced931948e84f61
Sumanth1727 <73636498+Sumanth1727@users.noreply.github.com>
2020-12-16 13:23:05 +0530

    Final Update

--- forms.py
+++ forms.py
@@ -3,7 +3,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, DateTimeField,RadioField,SelectField,TextField,TextAreaField,SubmitField
 from wtforms.validators import DataRequired,Email,EqualTo,Length
 class createcustomer(FlaskForm):
-    name=StringField('Name',validators=[DataRequired()])
+    firstname=StringField('First Name',validators=[DataRequired()])
+    lastname=StringField('Lst Name',validators=[DataRequired()])
+    gender=SelectField('Gender',choices=[('Male', 'Male'), ('Female', 'Female'),('Other', 'Other')],validators=[DataRequired()])
     email=StringField('Email',validators=[DataRequired(),Email()])
     phone=StringField('Phone Number',validators=[DataRequired()])
     balance=StringField('Current Desposited Amount')
@@ -12,7 +14,9 @@ class createcustomer(FlaskForm):
     submit=SubmitField("create")
 
 class createemloyee(FlaskForm):
-    name=StringField('Name',validators=[DataRequired()])
+    firstname=StringField('First Name',validators=[DataRequired()])
+    lastname=StringField('Lst Name',validators=[DataRequired()])
+    gender=SelectField('Gender',choices=[('Male', 'Male'), ('Female', 'Female'),('Other', 'Other')],validators=[DataRequired()])
     email=StringField('Email',validators=[DataRequired(),Email()])
     phone=StringField('Phone Number',validators=[DataRequired()])
     age=StringField('Age',validators=[DataRequired()])
@@ -24,9 +28,25 @@ class searchform(FlaskForm):
     submit=SubmitField("Search")
 
 class EditEmployee(FlaskForm):
-    name=StringField('Name')
+    firstname=StringField('First Name')
+    lastname=StringField('Lst Name')
+    gender=SelectField('Gender',choices=[('Male', 'Male'), ('Female', 'Female'),('Other', 'Other')])
     phone=StringField('Phone Number')
     balance=StringField('Current Desposited Amount')
     age=StringField('Age')
     address=TextAreaField('Address')
     submit=SubmitField("Edit")
+
+class RequestCards(FlaskForm):
+    CardType=SelectField('Card Type',choices=[('Master Card', 'Master Card'), ('Visa', 'Visa'),('rupee', 'Other')])
+    CardStatusType=SelectField('Card Status Type',choices=[('Debit Card', 'Debit Card'), ('Credit Card', 'Credit Card')])
+    Usagetype=SelectField('Usagetype',choices=[('Domestic', 'Domestic'), ('International', 'International')])
+    submit=SubmitField("Submit Request")
+
+class AmountTransferForm(FlaskForm):
+    Amount=StringField('Enter Amount of money to transfer',validators=[DataRequired()])
+    submit=SubmitField("Transfer")
+
+class EvaluationForm(FlaskForm):
+    Approve=SubmitField("Approve")
+    Decline=SubmitField("Decline")
--- tables.py
+++ tables.py
@@ -36,6 +36,7 @@ class B_Employee(db.Model):
 
     # If you don't provide this, the default table name will be the class name
     __tablename__ = 'B_Employee'
+    __table_args__ = {'extend_existing': True}
 
     # Now create the columns
     # Lots of possible types. We'll introduce through out the course
@@ -47,7 +48,9 @@ class B_Employee(db.Model):
 
     # Primary Key column, unique id for each puppy
     E_id = db.Column(db.Text,primary_key=True)
-    E_name = db.Column(db.Text)
+    E_firstname = db.Column(db.Text)
+    E_lastname = db.Column(db.Text)
+    E_gender = db.Column(db.Text)
     E_age = db.Column(db.Integer)
     E_email=db.Column(db.Text,unique=True)
     E_address=db.Column(db.Text)
@@ -56,10 +59,12 @@ class B_Employee(db.Model):
 
     # This sets what an instance in this table will have
     # Note the id will be auto-created for us later, so we don't add it here!
-    def __init__(self,E_id,name,age,email,address,number,E_pass):
+    def __init__(self,E_id,firstname,lastname,gender,age,email,address,number,E_pass):
 
         self.E_id = E_id
-        self.E_name = name
+        self.E_firstname =firstname
+        self.E_lastname = lastname
+        self.E_gender = gender
         self.E_age = age
         self.E_email=email
         self.E_address=address
@@ -71,7 +76,7 @@ class B_Employee(db.Model):
 
         # This is the  representation of B_Employee  in the model
 
-        m=[self.E_id,self.E_name,self.E_age,self.E_email,self.E_address,self.E_number,self.E_pass]
+
         return f"employee fetched"
 
 
@@ -89,8 +94,11 @@ class B_Employee(db.Model):
 class B_Customer(db.Model):
 
     __tablename__ = 'B_Customer'
+    __table_args__ = {'extend_existing': True}
     C_id = db.Column(db.Text,primary_key=True)
-    C_name = db.Column(db.Text)
+    C_firstname = db.Column(db.Text)
+    C_lastname = db.Column(db.Text)
+    C_gender = db.Column(db.Text)
     C_age = db.Column(db.Integer)
     C_email=db.Column(db.Text,unique=True)
     C_address=db.Column(db.Text)
@@ -101,10 +109,12 @@ class B_Customer(db.Model):
     C_Transacs = db.relationship('B_Customer_transactions',backref='B_Customer',lazy='dynamic')
 
 
-    def __init__(self,C_id,name,age,email,address,number,C_pass,balance):
+    def __init__(self,C_id,firstname,lastname,gender,age,email,address,number,C_pass,balance):
 
         self.C_id = C_id
-        self.C_name = name
+        self.C_firstname = firstname
+        self.C_lastname = lastname
+        self.C_gender = gender
         self.C_age = age
         self.C_email=email
         self.C_address=address
@@ -114,7 +124,7 @@ class B_Customer(db.Model):
 
 
     def __repr__(self):
-        a=[self.C_id,self.C_name,self.C_age,self.C_email,self.C_address,self.C_number,self.C_pass,self.C_balance]
+
         return f"customer fetched"
 
 
@@ -129,15 +139,20 @@ class B_Customer(db.Model):
 
 class B_Customer_transactions(db.Model):
     __tablename__ = 'B_Customer_transactions'
+    __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer,primary_key=True)
-    C_id = db.Column(db.Text,db.ForeignKey('B_Customer.C_id'))
     T_id = db.Column(db.Integer)
+    C_id = db.Column(db.Text,db.ForeignKey('B_Customer.C_id'))
+    R_id=db.Column(db.Text)
     Card_Number=db.Column(db.Integer,db.ForeignKey('B_Customer_Card_Details.Card_No'))
     T_amount=db.Column(db.Float)
     T_Type=db.Column(db.Text)
-    R_id=db.Column(db.Text)
+    T_Time=db.Column(db.Text)
+    T_balance=db.Column(db.Integer)
 
-    def __init__(self,C_id,T_id,Card_Number,T_amount,T_Type,R_id):
+
+    def __init__(self,C_id,R_id,Card_Number,T_amount,T_Type,T_id,T_Time,T_balance):
+
 
         self.C_id = C_id
         self.T_id = T_id
@@ -145,6 +160,8 @@ class B_Customer_transactions(db.Model):
         self.T_amount=T_amount
         self.T_Type=T_Type
         self.R_id=R_id
+        self.T_Time=T_Time
+        self.T_balance=T_balance
 
 
     def __repr__(self):
@@ -161,10 +178,14 @@ class B_Customer_transactions(db.Model):
 class B_Customer_Card_Details(db.Model):
 
     __tablename__ = 'B_Customer_Card_Details'
+    __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer,primary_key=True)
     C_id = db.Column(db.Text,db.ForeignKey('B_Customer.C_id'))
     Card_Type = db.Column(db.Text)
     Card_No = db.Column(db.Integer,unique=True)
+    Usagetype=db.Column(db.Text)
+    Card_Status_Type=db.Column(db.Text)
+    ExpiryDate=db.Column(db.Text)
     Card_Activate=db.Column(db.Boolean)
     # add Expiry date to card details
     #add access type to card details
@@ -172,17 +193,77 @@ class B_Customer_Card_Details(db.Model):
 
 
 
-    def __init__(self,C_id,Card_Type,Card_No,Card_Activate):
+    def __init__(self,C_id,Card_Type,Card_No,Card_Activate,Usagetype,Card_Status_Type,ExpiryDate):
 
         self.C_id = C_id
         self.Card_Type = Card_Type
         self.Card_No=Card_No
         self.Card_Activate=Card_Activate
+        self.Card_Status_Type=Card_Status_Type
+        self.ExpiryDate=ExpiryDate
+        self.Usagetype=Usagetype
 
 
     def __repr__(self):
         return [self.id,self.self.self.self.C_id,self.self.self.Card_Type,self.self.Card_No,self.Card_Activate]
 
+
+class CardRequests(db.Model):
+
+    __tablename__ = 'CardRequests'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer,primary_key=True)
+    C_id = db.Column(db.Text,)
+    Card_Type = db.Column(db.Text)
+    Usagetype=db.Column(db.Text)
+    Card_Status_Type=db.Column(db.Text)
+    Request_Status=db.Column(db.Text)
+    # add Expiry date to card details
+    #add access type to card details
+
+    def __init__(self,C_id,Card_Type,Usagetype,Card_Status_Type,Request_Status):
+
+        self.C_id = C_id
+        self.Card_Type = Card_Type
+        self.Card_Status_Type=Card_Status_Type
+        self.Usagetype=Usagetype
+        self.Request_Status=Request_Status
+
+
+    def __repr__(self):
+        return "Card Created"
+
+
+class logs(db.Model):
+    __tablename__ = 'logs'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer,primary_key=True)
+    Actor_id = db.Column(db.Text)
+    Actor_Type = db.Column(db.Text)
+    ActionDone = db.Column(db.Text)
+    OtherId=db.Column(db.Text)
+    ActionTime=db.Column(db.Text)
+
+    def __init__(self,Actor_id,Actor_Type,ActionDone,OtherId,ActionTime):
+
+        self.Actor_id = Actor_id
+        self.Actor_Type = Actor_Type
+        self.ActionDone=ActionDone
+        self.OtherId=OtherId
+        self.ActionTime=ActionTime
+
+class CardDeActLogs(db.Model):
+    __tablename__ = 'CardDeActLogs'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer,primary_key=True)
+    C_id = db.Column(db.Text)
+    CardNo = db.Column(db.Integer,unique=True)
+    DeActReason = db.Column(db.Text)
+
+    def __init__(self,C_id,CardNo,DeActReason):
+        self.C_id = C_id
+        self.CardNo = CardNo
+        self.DeActReason=DeActReason
 
 #creating Table instances
 db.create_all()
